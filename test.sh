00d7f1beee61d23f4b62284d3e7455e96dbdd8ff
ASVBPREAUBV <paul@paul-mayer.eu>
2018-12-27 02:06:32 +0100

    nginx deamon test

--- test.sh
+++ test.sh
@@ -3,6 +3,19 @@
 
 docker rm $(docker ps -a -q) -f
 
+
+docker run \
+-p 9000:80 \
+-d \
+--net fastnetwork \
+-l traefik.docker.network=fastnetwork \
+-l traefik.enable=true \
+-l traefik.basic.frontend.rule=Host:pfaulk.de \
+-l traefik.basic.port=9000 \
+-l traefik.basic.protocol=https \
+--name html \
+nginx
+
 docker run \
 -p 8080:8080 \
 -p 80:80 \
@@ -13,14 +26,3 @@ docker run \
 --net fastnetwork \
 traefik
 
-
-docker run \
--p 9000:80 \
---net fastnetwork \
--l traefik.docker.network=fastnetwork \
--l traefik.enable=true \
--l traefik.basic.frontend.rule=Host:pfaulk.de \
--l traefik.basic.port=9000 \
--l traefik.basic.protocol=https \
---name html \
-nginx

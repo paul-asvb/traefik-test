6ac4fe30a52020ff04999e7e57eea594a620db14
ASVBPREAUBV <paul@paul-mayer.eu>
2018-12-27 03:37:46 +0100

    remove docker network use

--- test.sh
+++ test.sh
@@ -13,30 +13,31 @@ containous/whoami
 
 echo "whoami running"
 
-docker run \
--p 9000:80 \
--d \
---net fastnetwork \
--l traefik.docker.network=fastnetwork \
--l traefik.enable=true \
--l traefik.frontend.entryPoints=http \
--l traefik.basic.frontend.rule=Host:pfaulk.de \
--l traefik.basic.port=9000 \
--l traefik.basic.protocol=http \
---name html \
-nginx
-
-echo "nginx running"
+#
+#docker run \
+#-p 9000:80 \
+#-d \
+#--net fastnetwork \
+#-l traefik.docker.network=fastnetwork \
+#-l traefik.enable=true \
+#-l traefik.frontend.entryPoints=http \
+#-l traefik.basic.frontend.rule=Host:pfaulk.de \
+#-l traefik.basic.port=9000 \
+#-l traefik.basic.protocol=http \
+#--name html \
+#nginx
+#
+#echo "nginx running"
 
 docker run \
 -d \
 -p 8080:8080 \
 -p 80:80 \
--p 443:443 \
 -v $PWD/traefik.toml:/etc/traefik/traefik.toml \
 -v /var/run/docker.sock:/var/run/docker.sock \
---net fastnetwork \
 traefik
 
+# -p 443:443 \
+# --net fastnetwork \
 # -v $PWD/acme.json:/acme.json
 

1b49ea755a936b4b93e29fed5dc55c16e6be71d6
ASVBPREAUBV <paul@paul-mayer.eu>
2018-12-28 03:50:50 +0100

    test with letsencript no deamon

--- test.sh
+++ test.sh
@@ -31,12 +31,13 @@ nginx
 #--net fastnetwork \
 
 docker run \
--d \
 -p 8080:8080 \
 -p 80:80 \
 -v $PWD/traefik.toml:/etc/traefik/traefik.toml \
 -v /var/run/docker.sock:/var/run/docker.sock \
 traefik
+
+# -d \
 
 # -p 443:443 \
 # --net fastnetwork \

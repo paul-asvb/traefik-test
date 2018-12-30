99b4bd87ff20f9a0470e4a2273aacebce2cf656f
ASVBPREAUBV <paul@paul-mayer.eu>
2018-12-30 03:49:17 +0100

    init docker and node app

---
+++ src/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+
+app.get('/hello', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.get('/*', function (req, res) {
+    res.send(req.path);
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
